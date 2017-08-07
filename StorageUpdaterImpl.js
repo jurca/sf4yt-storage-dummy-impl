f95e3e550676f650f219a184725cdd6cd66cc537
Martin Jurča <mjurca@centrum.cz>
2017-08-07 18:44:20 +0200

    implemented resolving accounts

--- StorageUpdaterImpl.js
+++ StorageUpdaterImpl.js
@@ -2,19 +2,26 @@
 
 import type {StorageUpdater} from 'sf4yt-storage/StorageUpdater'
 import type {StorageDataSource} from 'sf4yt-storage/StorageDataSource'
+import type {Account} from 'sf4yt-storage/model/Account'
+import RecordStorage from './storage/RecordStorage'
 
 export default class StorageUpdaterImpl {
+  _accounts: RecordStorage<Account, string>
   _dataSource: StorageDataSource
 
-  constructor(dataSource: StorageDataSource) {
+  constructor(
+    accounts: RecordStorage<Account, string>,
+    dataSource: StorageDataSource
+  ) {
+    this._accounts = accounts
     this._dataSource = dataSource
 
     Object.freeze(this)
   }
 
   async resolveAccount(accountId: string): Promise<void> {
-    const resolvedAccount = this._dataSource.resolveAccount(accountId)
-    // TODO
+    const resolvedAccount = await this._dataSource.resolveAccount(accountId)
+    await this._accounts.persist(resolvedAccount)
   }
 
   updateSubscriptions(): Promise<void> {}
